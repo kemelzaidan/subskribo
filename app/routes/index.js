198ace6d8e4c7196b01e02e8ef52fbcc6052b2c3
Kemel Zaidan <kemelzaidan@gmail.com>
2015-12-09 17:10:03 -0200

    change app/routes/index.js to my defaults

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,13 +4,13 @@ export default Ember.Route.extend({
   // this.route('index', { path: '/' });
   model(params) {
     return Ember.Object.create ({
-      nome: "João Silva",
-      cargo: "Desenvolvedor web",
-      email: "joao.silva@locaweb.com.br",
-      telefone: "11 33420718",
-      ramal: "207",
-      celular: "11 97174-7414",
-      skype: "joao_silva"
+      nome: "Kemel Zaidan",
+      cargo: "Evangelista locaweb",
+      email: "kemel.zaidan@locaweb.com.br",
+      telefone: "11 3544-0444",
+      ramal: "270",
+      celular: "11 97174-0000",
+      skype: "kemel_zaidan"
     })
   }
 });
