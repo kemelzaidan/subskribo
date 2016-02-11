a15803a48a35356d1d959edeb582a51b4d194394
Kemel Zaidan <kemelzaidan@gmail.com>
2016-02-11 11:12:00 -0200

    initial gh-pages commit

---
+++ app/routes/index.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  // this.route('index', { path: '/' });
+  model() {
+    return Ember.Object.create ({
+      nome: "Kemel Zaidan",
+      cargo: "Evangelista Locaweb",
+      email: "kemel.zaidan@locaweb.com.br",
+      telefone: "11 3544-0444",
+      ramal: "270",
+      celular: "11 97174-0000",
+      skype: "kemel_zaidan"
+    });
+  }
+});
---
+++ config/environment.js
@@ -0,0 +1,59 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'subskribo',
+    environment: environment,
+    baseURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' assets.locaweb.com.br",
+      'font-src': "'self' assets.locaweb.com.br fonts.gstatic.com",
+      'connect-src': "'self'",
+      'img-src': "'self' assets.locaweb.com.br www.gravatar.com htmlsig.com www.companysig.com",
+      'style-src': "'self' 'unsafe-inline' assets.locaweb.com.br fonts.googleapis.com",
+      'media-src': "'self'"
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.baseURL = '/subskribo';
+
+  }
+
+  return ENV;
+};
